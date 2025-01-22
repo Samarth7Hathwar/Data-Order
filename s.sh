7009ab33688e33305c4541dcec4306106550d27a
Samarth M Hathwar <89646111+Samarth7Hathwar@users.noreply.github.com>
2025-01-22 15:33:42 +0100

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name="imagenet_training"
-#SBATCH --partition=batch
+#SBATCH --job-name="Imagenet_training"
+#SBATCH --partition=A100-80GB,A100-PCI,A100-40GB
 #SBATCH --gpus=3
-#SBATCH --cpus-per-gpu=50
+#SBATCH --cpus-per-gpu=10
 #SBATCH --mem=250G
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 
 srun \
   --container-image=/enroot/nvcr.io_nvidia_pytorch_23.12-py3.sqsh \
   --container-mounts=/ds:/ds:ro,"`pwd`":"`pwd`" \
   --container-workdir="`pwd`" \
-  bash -c "python3 -m pip install timm scikit-learn && python3 project4.py"
+  bash -c "python3 -m pip install --upgrade timm pip torchvision scikit-learn && python3 project7.py"
