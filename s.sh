4e2f85e45acca80a03d17eeba5e0eb227682ca56
Samarth M Hathwar <89646111+Samarth7Hathwar@users.noreply.github.com>
2025-02-02 23:41:54 +0100

    Updated for 4 GPUs

--- s.sh
+++ s.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name="Imagenet_training"
 #SBATCH --partition=A100-80GB,A100-PCI,A100-40GB
-#SBATCH --gpus=3
+#SBATCH --gpus=4
 #SBATCH --cpus-per-gpu=10
-#SBATCH --mem=250G
+#SBATCH --mem=150G
 #SBATCH --time=72:00:00
+
+export PATH=$HOME/.local/bin:$PATH
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export NCCL_DEBUG=WARN
+export TORCH_DISTRIBUTED_DEBUG=OFF
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
 
 srun \
   --container-image=/enroot/nvcr.io_nvidia_pytorch_23.12-py3.sqsh \
   --container-mounts=/ds:/ds:ro,"`pwd`":"`pwd`" \
   --container-workdir="`pwd`" \
-  bash -c "python3 -m pip install --upgrade timm pip torchvision scikit-learn && python3 project7.py"
+  bash -c "python3 -m pip install --upgrade timm pip torchvision scikit-learn && \
+           torchrun --standalone  --nproc_per_node=4 project8.py"
