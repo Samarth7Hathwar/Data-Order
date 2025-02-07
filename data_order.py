9e6e0cc297c91928b010ff6c1a893a40f5eed794
Samarth M Hathwar <89646111+Samarth7Hathwar@users.noreply.github.com>
2025-02-07 13:50:49 +0100

    Updated code

--- data_order.py
+++ data_order.py
@@ -35,11 +35,12 @@ transform = transforms.Compose([
 ])
 
 val_transform = transforms.Compose([
-    transforms.CenterCrop(224),  # Standard center crop
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
 ])
-
 
 # Checking if the dataset directories exist
 assert os.path.exists(train_dir), f"Training directory not found: {train_dir}"
